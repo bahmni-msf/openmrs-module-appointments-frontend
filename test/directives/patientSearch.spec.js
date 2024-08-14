f1bed427c530ff60d612355f081a84e41c5db3f0
rsrilaxmiragini <srilaxmiragini.r@thoughtworks.com>
2024-08-14 11:31:23 +0530

    Sri Laxmi | MOBN-2465 | Update. patientservice search with lucene.

--- test/directives/patientSearch.spec.js
+++ test/directives/patientSearch.spec.js
@@ -6,8 +6,8 @@ describe("Patient Search", function () {
     beforeEach(module('bahmni.appointments', function ($provide) {
         $location = jasmine.createSpyObj('$location', ['search']);
         $location.search.and.returnValue({});
-        patientService = jasmine.createSpyObj('patientService', ['search','getPatient']);
-        patientService.search.and.returnValue(specUtil.simplePromise({data: {}}));
+        patientService = jasmine.createSpyObj('patientService', ['search','getPatient','luceneSearch']);
+        patientService.luceneSearch.and.returnValue(specUtil.simplePromise({data: {}}));
         spinner = jasmine.createSpyObj('spinner', ['forPromise']);
         spinner.forPromise.and.callFake(function (param) {
             return {
